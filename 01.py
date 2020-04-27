b9ece229c1f4843a5bed36045d3951465747f8ac
dongsw-gif <64344717+dongsw-gif@users.noreply.github.com>
2020-04-27 22:27:14 +0800

    'dss'

---
+++ 01.py
@@ -0,0 +1,17 @@
+"""
+Author:董仕伟
+QQ:842785707
+Time:2020/4/24 12:48
+Company:科信技术有限公司
+"""
+
+import  time
+from selenium import webdriver
+
+
+
+driver = webdriver.Chrome()
+driver.get("http://baidu.com")
+time.sleep(10)
+
+driver.quit()
